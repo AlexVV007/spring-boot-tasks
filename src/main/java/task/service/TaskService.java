fd83c16cd057ee14d5b81c148ad409fe71c793ed
Ващилко Алексей <aleksejvv17@gmail.com>
2025-09-27 23:51:05 +0300

    add task 7

--- src/main/java/task/service/TaskService.java
+++ src/main/java/task/service/TaskService.java
@@ -1,23 +1,33 @@
 package task.service;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import task.model.Task;
 import task.repository.TaskRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final KafkaTemplate<String, Task> kafkaTemplate;
 
-    public TaskService(TaskRepository taskRepository) {
+
+    public TaskService(TaskRepository taskRepository, KafkaTemplate<String, Task> kafkaTemplate) {
         this.taskRepository = taskRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
+
 
     public List<Task> getAllTasks() {
         return taskRepository.findByIsDeleteFalse();
@@ -41,4 +51,19 @@ public class TaskService {
     public void deleteTask(Long id) {
         taskRepository.markAsDeleted(id);
     }
+
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.SECONDS)
+    public void checkForOverdueTasks() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Task> activeTask = taskRepository.findByIsDeleteFalse();
+        System.out.printf("Total active task: %d\n",activeTask.size());
+    }
+
+
+    @Async
+    public CompletableFuture<List<Task>> getCompletedUserTasks(Long userId) {
+        List<Task> retrieval = taskRepository.findByUserIdAndIsCompleteFalseAndIsDeleteFalse(userId);
+        return CompletableFuture.completedFuture(retrieval);
+    }
+
 }
