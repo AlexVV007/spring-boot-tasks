b439813723d630fb4200f25182e2c16067e5c570
Ващилко Алексей <aleksejvv17@gmail.com>
2025-09-29 17:32:48 +0300

    Corrected for creation Merge requests

--- src/main/java/task/service/TaskService.java
+++ src/main/java/task/service/TaskService.java
@@ -1,7 +1,5 @@
 package task.service;
 
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
